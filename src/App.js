1e5afc26c9c8d2abfc7995db4aeacef9c7782277
nagasritha <eernagasritha@gmail.com>
2023-07-27 22:02:59 +0530

    used component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import UserDetails from './Components/UserDetails';
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload successfully.
         </p>
+        <UserDetails/>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/Components/UserDetails/index.js
@@ -0,0 +1,4 @@
+import './index.css'
+ const UserDetails=()=><h1>hello world</h1>
+ 
+ export default UserDetails
