f95b05629bde0eb01754dfd1e29c35a775fda306
nagasritha <eernagasritha@gmail.com>
2023-07-27 22:21:42 +0530

    main project

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
-import logo from './logo.svg';
-import UserDetails from './Components/UserDetails';
-import './App.css';
+import UserDetials from './Components/UserDetails/index'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload successfully.
-        </p>
-        <UserDetails/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App=()=><div>
+    <UserDetials/>
+  </div>
 
-export default App;
+export default App
